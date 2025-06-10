7a938332d6ca5af73bc61a531a312efc5f187dca
Kiragotzz <luis_gabriell1@hotmail.com>
2025-06-10 20:16:57 -0300

    added cors policies

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+     allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["Authorization", "Content-Type"],
+    expose_headers=["X-Custom-Header"],  # Headers expostos ao navegador
+    max_age=600  # Tempo de cache das configurações CORS (em segundos)
+)
 
 darksouls2 = {
   "areas": [
